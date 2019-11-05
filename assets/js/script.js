900a01d7313ef296851ae837a60734b6a75fab41
francois80 <glavieux.francois@orange.fr>
2019-11-05 10:01:53 +0100

    ajout exercice 2 javascript partie 3

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+function fAffiche(id){
+//  var id = id;
+  document.getElementById(id).style.display = "block";
+}
+
+function fMasque(id){
+//var id = id;
+  document.getElementById(id).style.display = "none";
+}
